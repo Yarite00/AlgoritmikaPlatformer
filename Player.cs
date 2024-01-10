588661f22d7c9cc3a977a540959ef8224d840314
Yarite00 <81412723+Yarite00@users.noreply.github.com>
2024-01-10 13:51:28 +0400

    Add files via upload

---
+++ Player.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Player : MonoBehaviour
+{
+    private int health = 10;
+    private int coins;
+    public GameObject fireballPrefab;
+    public Transform attackPoint;
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        print("Здоровье игрока: " + health);
+    }
+    public void CollectCoins()
+    {
+        coins++;
+        print("Собранные монетки: " + coins);
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Instantiate(fireballPrefab, attackPoint.position, attackPoint.rotation);
+        }
+    }
+}
