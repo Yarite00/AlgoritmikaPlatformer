76b736ae0e83f2aaf86ac63d6a9fb11f2a983b20
Yarite00 <81412723+Yarite00@users.noreply.github.com>
2024-01-10 14:25:25 +0400

    Add files via upload

---
+++ TimerUI.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+public class Timer : MonoBehaviour
+{
+    public int minutes;
+    public float seconds;
+    public TextMeshPro timerText;
+    void Update()
+    {
+        seconds -= Time.deltaTime;
+        if (seconds <= 0)
+        {
+            if (minutes > 0)
+            {
+                seconds += 59;
+                minutes--;
+            }
+            else
+            {
+                int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+                SceneManager.LoadScene(sceneIndex);
+            }
+        }
+        int roundSeconds = Mathf.RoundToInt(seconds);
+        timerText.text = minutes + ":" + roundSeconds;
+    }
+}
